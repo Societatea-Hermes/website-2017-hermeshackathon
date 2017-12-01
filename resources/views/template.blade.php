8fa7da1ceed10fd4425aeae462ec8a8c351a280f
Porutiu Flaviu <flaviu@glitch.ro>
2017-12-01 15:28:15 +0200

    Added hotjar

--- resources/views/template.blade.php
+++ resources/views/template.blade.php
@@ -29,6 +29,18 @@ $isProduction = App::environment() == 'production' ? true : false;
 			<script src="http://css3-mediaqueries-js.googlecode.com/svn/trunk/css3-mediaqueries.js"></script>
 		<![endif]-->
 
+		<!-- Hotjar Tracking Code for hackathon.societatea-hermes.ro -->
+		<script>
+		    (function(h,o,t,j,a,r){
+		        h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+		        h._hjSettings={hjid:709809,hjsv:6};
+		        a=o.getElementsByTagName('head')[0];
+		        r=o.createElement('script');r.async=1;
+		        r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+		        a.appendChild(r);
+		    })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+		</script>
+
 		@yield('extraCss')
 
 		<!-- Document Title
